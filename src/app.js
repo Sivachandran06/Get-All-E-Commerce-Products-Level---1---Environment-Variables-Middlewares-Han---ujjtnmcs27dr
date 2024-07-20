47aade985cf9bf522dc714d639cacb21a2f9c47f
sivachandran06 <sivachandran997@gamil.com>
2024-07-20 21:24:33 +0530

    change

--- src/app.js
+++ src/app.js
@@ -8,7 +8,27 @@ const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 // Middlewares
 app.use(express.json());
 
+
 // Write GET endpoint for sending all the products to client here
 // Endpoint - /api/v1/products
+
+app.get("/api/v1/products",(req,res)=>{
+    if(products && products.length){
+        res.statusCode = 200;
+        const payLoad = {
+            status: "success", 
+            message: "Product fetched successfully",
+            data: { 
+                products,
+            },
+        };
+        res.json(payLoad);
+
+    }else{
+        res.statusCode = 404;
+        const payLoad = { message:"Product not found"};
+        res.json(payLoad);
+    }
+});
 
 module.exports = app;
